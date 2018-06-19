87e3caf4aff0b3f3c6d90421fa9d6f33891de03e
Jason Bockmon <jbockmon@gmail.com>
2018-06-18 21:38:28 -0500

    announcements for joins added

--- server/server.js
+++ server/server.js
@@ -11,10 +11,24 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-   console.log('New User Connected!');
+   socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app!',
+      createdAt: new Date().getTime()
+   });
+
+   socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: "So and so joined the room",
+      createdAt: new Date().getTime()
+   });
 
    socket.on('createMessage', (message) => {
-      console.log(message)
+      io.emit('newMessage', {
+         from: message.from,
+         text: message.text,
+         createdAt: new Date().getTime()
+      });
    })
 
    socket.on('disconnect', () => {
