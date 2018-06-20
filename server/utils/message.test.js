c20fef7e59a3e6435f3b722c5a14fbac29006135
Jason Bockmon <jbockmon@gmail.com>
2018-06-20 14:36:49 -0500

    added geolocation support and updated testing files.

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,19 +1,32 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
-describe('generageMessage', () => {
-    it('should generate the correct message object', () => {
-        var testObj = {
-            'from': 'sender',
-            'text': 'message text here'
-        }
-        var message = generateMessage(testObj.from, testObj.text);
+describe('generateMessage', () => {
+   it('should generate the correct message object', () => {
+      var testObj = {
+         from: 'sender',
+         text: 'message text here'
+      }
+      var message = generateMessage(testObj.from, testObj.text);
 
-        expect(message.from).toBe(testObj.from);
-        expect(message.text).toBe(testObj.text);
-        expect(typeof message.createdAt).toEqual('number');
-
-
-    });
+      expect(message.from).toBe(testObj.from);
+      expect(message.text).toBe(testObj.text);
+      expect(typeof message.createdAt).toEqual('number');
+   });
 });
+
+describe('generateLocationMessage', () => {
+   it('should generate the correct location object', () => {
+      var testObj = {
+         from: 'sender',
+         url: `https://www.google.com/maps?q=50,50`,
+         createdAt: new Date().getTime()
+      };
+      var message = generateLocationMessage(testObj.from, 50, 50);
+
+      expect(message.from).toBe(testObj.from);
+      expect(message.url).toBe(testObj.url);
+      expect(typeof message.createdAt).toEqual('number');
+   });
+})
